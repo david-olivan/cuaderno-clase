0477e3453e1a3ad5a5113bac74c49dcb883eacba
David Oliván Malagón <david.olivan.93@gmail.com>
2025-06-02 10:00:07 +0200

    v2 Primera ronda

--- 2-tareas front v2/src/app.js
+++ 2-tareas front v2/src/app.js
@@ -1,16 +1,35 @@
 // TODO: implementar persistencia de tareas
 
 // TODO: Rehacer el diseño del código para utilizar nombres más ilustrativos
-// olvidándonos de main.
 
 // TODO: desacoplar funciones comunes
 
-const tareasPendientes = ["Limpiar ordenador","Explicar JS", "Resolver dudas"]
-const tareasCompletas = []
 
-function main() {
-    renderApp()
+/**
+ * Guarda una lista de tareas en localStorage
+ * @param {string[]} lista lista de tareas para guardar
+ */
+function guardarTareas(lista) {
+    localStorage.setItem("tareas", lista)
 }
+
+
+/**
+ * Devuelve la lista de tareas guardadas en localStorage
+ * @returns {string[]}
+ */
+function cargarTareas() {
+    let tareas = localStorage.getItem("tareas")
+    console.log(typeof tareas)
+    
+    if (!tareas) {
+        tareas = []
+        guardarTareas(tareas)
+    }
+
+    return tareas
+}
+
 
 function renderApp() {
     const myApp = document.getElementById("app")    
@@ -21,50 +40,45 @@ function renderApp() {
 
         <h3>Tareas pendientes</h3>
         <ol>
-            ${formatearTareas(tareasPendientes, completo=false)}
+            ${formatearTareas(cargarTareas())}
         </ol>
-
-        <h3>Tareas Completas</h3>
-        <ul>
-            ${formatearTareas(tareasCompletas, completo=true)}
-        </ul>
     `
 }
 
 
-function crearTarea() {
-    // TODO: Cambiar la estructura de datos a un modelo más flexible
-    const userInput = document.getElementById("nombreTarea")
-    tareasPendientes.push(userInput.value)
-    userInput.value = ""   
-    renderApp()
-}
+// function crearTarea() {
+//     // TODO: Cambiar la estructura de datos a un modelo más flexible
+//     const userInput = document.getElementById("nombreTarea")
+//     tareasPendientes.push(userInput.value)
+//     userInput.value = ""   
+//     renderApp()
+// }
 
 
-function formatearTareas(listaDeTareas, completo) {
+function formatearTareas(listaDeTareas) {
     const tareasFormateadas = []
 
     listaDeTareas.forEach((tarea) => {
-        tareasFormateadas.push(`<li ${completo ? "class='tachado'": ""}>${tarea}</li>`)
+        tareasFormateadas.push(`<li>${tarea}</li>`)
     })
 
     return tareasFormateadas.join("")
 }
 
 
-function eliminarTarea() {
-    const idTarea = document.getElementById("eliminarTarea")
+// function eliminarTarea() {
+//     const idTarea = document.getElementById("eliminarTarea")
 
-    if (!tareasPendientes[Number(idTarea.value - 1)]) {
-        alert("Tarea no existe")
-    } else {
-        tareasCompletas.push(tareasPendientes.splice(Number(idTarea.value - 1), 1))
-    }
-
-
-    idTarea.value = ""
-    renderApp()
-}
+//     if (!tareasPendientes[Number(idTarea.value - 1)]) {
+//         alert("Tarea no existe")
+//     } else {
+//         tareasPendientes.splice(Number(idTarea.value - 1), 1)
+//     }
 
 
-main()
+//     idTarea.value = ""
+//     renderApp()
+// }
+
+
+renderApp()
