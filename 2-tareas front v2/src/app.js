d21acd6984576714617fd41ad0ababf074da781b
David Oliván Malagón <david.olivan.93@gmail.com>
2025-06-02 14:03:03 +0200

    Versión final lunes

--- 2-tareas front v2/src/app.js
+++ 2-tareas front v2/src/app.js
@@ -1,33 +1,64 @@
-// TODO: implementar persistencia de tareas
-
-// TODO: Rehacer el diseño del código para utilizar nombres más ilustrativos
-
-// TODO: desacoplar funciones comunes
+// ESTRUCTURA DE TAREAS
+// {
+//  nombre, prioridad, estado
+// }
 
 
 function renderApp() {
-    const myApp = document.getElementById("app")    
+    const tablaTareas = document.getElementById("tablaTareas")
+    tablaTareas.innerHTML = ""
 
-    // TODO: crear los componentes de tareas de manera programática
-    myApp.innerHTML = `
-        <h2>Bienvenid@ a la App</h2>
-
-        <h3>Tareas pendientes</h3>
-        <ol>
-            ${formatearTareas(cargarTareas())}
-        </ol>
-    `
+    cargarTareas().forEach((tarea, index) => {
+        tablaTareas.innerHTML += `
+                <tr>
+                    <td>${index + 1}</td>
+                    <td>${tarea.nombre}</td>
+                    <td>Importante</td>
+                    <td>En progreso</td>
+                </tr>
+        `
+    })
 }
 
+/**
+ * Hace una llamada a añadirTareas y resetea el valor del input
+ */
+function crearTarea() {
+    const userInput = document.getElementById("nombreTarea")
+    añadirTarea(userInput.value)
+    userInput.value = ""
 
-// function crearTarea() {
-//     // TODO: Cambiar la estructura de datos a un modelo más flexible
-//     const userInput = document.getElementById("nombreTarea")
-//     tareasPendientes.push(userInput.value)
-//     userInput.value = ""   
-//     renderApp()
-// }
+    renderApp()
+}
 
+/**
+ * La función crea un objeto con las propiedades de tarea (nombre, prioridad, estado) y lo guarda en localStorage
+ * @param {string} nombre El nombre de la tarea
+ * @param {number} prioridad La prioridad de la tarea
+ * @param {string} estado El estado de la tarea
+ * @returns 
+ */
+function añadirTarea(nombre, prioridad=1, estado="pendiente") {
+    if (nombre.length < 3 || nombre.length > 63) {
+        console.warn("El nombre de la tarea debe tener entre 3 y 63 caracteres")
+        return
+    }
+
+    if (![1, 2 ,3].includes(prioridad)) {
+        console.warn("Prioridad no es válida")
+        return
+    }
+
+    if (!["pendiente", "progreso", "completa"].includes(estado)) {
+        console.warn("El estado debe ser 'pendiente', 'progreso' o 'completa'")
+        return
+    }
+
+    const tareas = cargarTareas()
+    tareas.push({nombre, prioridad, estado})
+
+    guardarTareas(tareas)
+}
 
 function formatearTareas(tareas) {
     const tareasFormateadas = []
@@ -40,19 +71,22 @@ function formatearTareas(tareas) {
 }
 
 
-// function eliminarTarea() {
-//     const idTarea = document.getElementById("eliminarTarea")
+function eliminarTarea() {
+    const userInput = document.getElementById("eliminarTarea")
+    const idTarea = Number(userInput.value) - 1
+    const tareas = cargarTareas()
+    userInput.value = ""
 
-//     if (!tareasPendientes[Number(idTarea.value - 1)]) {
-//         alert("Tarea no existe")
-//     } else {
-//         tareasPendientes.splice(Number(idTarea.value - 1), 1)
-//     }
+    if (!tareas[idTarea]) {
+        console.warn("Tarea no existe o el id no es válido")
+        return
+    }
+    
+    tareas.splice(idTarea, 1)
 
-
-//     idTarea.value = ""
-//     renderApp()
-// }
+    guardarTareas(tareas)    
+    renderApp()
+}
 
 
 renderApp()
