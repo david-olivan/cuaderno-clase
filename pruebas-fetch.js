673658cd5a0344a54de2f9b99b836e32a3ec4887
David Oliván Malagón <david.olivan.93@gmail.com>
2025-06-10 13:44:41 +0200

    pruebas para ejemplos

--- pruebas-fetch.js
+++ pruebas-fetch.js
@@ -1,11 +1,19 @@
 async function pruebasFetch() {
 	return fetch("https://jsonplaceholder.typicode.com/usjers/")
 		.then((response) => {
-            if (!response.ok) throw new Error(response.status)
-            return response.json()
-        })
-		.catch((error) => console.log(error.message));
+			if (!response.ok) throw new Error(response.status);
+			return response.json();
+		})
+		.catch((error) => error.message);
 }
 
 const pruebas = await pruebasFetch();
-console.log(pruebas);
+const pruebas2 = await fetch("https://jsonplaceholder.typicode.com/users/1")
+	.then((response) => {
+		if (!response.ok) throw new Error(response.status);
+		return response.json();
+	})
+	.catch((error) => error.message);
+
+console.log(pruebas)
+console.log(pruebas2)
