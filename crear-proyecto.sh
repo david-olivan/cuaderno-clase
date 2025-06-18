9e8f0e47aedae1daab9b9f500e22d14343b58c3b
David Oliván Malagón <david.olivan.93@gmail.com>
2025-06-18 14:44:51 +0200

    Bash y Astro

---
+++ crear-proyecto.sh
@@ -0,0 +1,2 @@
+mkdir -p proyecto/{assets/images,assets/fonts,src,dist,views,styles}
+touch proyecto/{dist/types.ts,src/main.js,src/tienda.js,styles/main.css,styles/tienda.css,views/tienda.html,index.html}
