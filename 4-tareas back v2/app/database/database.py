6c82a8f896620c5ac460fc95832bad699bae36bd
David Oliván Malagón <david.olivan.93@gmail.com>
2025-07-16 13:32:23 +0200

    feat: base de datos con POST y GET

--- 4-tareas back v2/app/database/database.py
+++ 4-tareas back v2/app/database/database.py
@@ -1,7 +1,10 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, Session
 
 
 DB_URL = "sqlite:///app/database/sqlite.db"
 
 engine = create_engine(DB_URL, echo=True)
 
+async def get_session():
+    with Session(engine) as session:
+        yield session
--- 4-tareas back v2/app/routes/tareas.py
+++ 4-tareas back v2/app/routes/tareas.py
@@ -1,8 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends
+from sqlmodel import Session, select
 from pydantic import BaseModel
 from ..controllers.tareas import get_tareas
+from ..database.database import get_session
+from ..database.models import Tarea as TareaDB
 
-class TareaRequest(BaseModel): ...
+class TareaRequest(BaseModel):
+    name: str
+    prioridad: int
+    asignado_a: str
+
 
 router = APIRouter(
     prefix="/tareas",
@@ -10,16 +17,24 @@ router = APIRouter(
 )
 
 @router.get("")
-async def leer_tareas():
-    return await get_tareas()
+async def leer_tareas(session: Session = Depends(get_session)):
+    operacion = select(TareaDB)
+    tareas = session.exec(operacion).all()
+    return tareas
 
 @router.get("/{tarea_id}")
-async def leer_tarea(tarea_id: int):
-    return {"message": "Ha funcionado"}
+async def leer_tarea(tarea_id: int, session: Session = Depends(get_session)):
+    tarea = session.get_one(TareaDB, tarea_id)
+    return tarea if tarea else {"message": "No hay tarea con este id"}
 
-@router.post("")
-async def crear_tarea(tarea: TareaRequest):
-    return {"message": "Ha funcionado"}
+@router.post("", status_code=status.HTTP_201_CREATED)
+async def crear_tarea(tarea: TareaRequest, session: Session = Depends(get_session)):
+    nueva_tarea: TareaDB = TareaDB(**tarea.model_dump())
+    # Cargar operación de guardado en la sesión
+    session.add(nueva_tarea)
+    # Guardar operaciones
+    session.commit()
+    return {"message": "Tarea guardada"}
 
 @router.put("/{tarea_id}")
 async def actualizar_tarea(tarea_id: int):
