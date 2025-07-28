c701b10dd669beb18b85dc81dcc397c0a4ccea82
David Oliván Malagón <david.olivan.93@gmail.com>
2025-07-28 14:16:05 +0200

    feat: Seeder añadido

--- 4-schoolapi v1/src/database.py
+++ 4-schoolapi v1/src/database.py
@@ -1,3 +1,5 @@
+import asyncio
+from faker import Faker
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import Document, init_beanie
 from typing import Optional
@@ -29,3 +31,33 @@ async def init_db():
     client = AsyncIOMotorClient(MONGO_URL)
     database = client["schoolapi"]
     await init_beanie(database=database, document_models=[Estudiante, Curso])  # type: ignore
+
+
+async def seed_everything():
+    await init_db()
+
+    fk = Faker()
+
+    student_input = int(input("Cuantos estudiantes quieres: "))
+    curso_input = int(input("Cuantos cursos quieres: "))
+
+    for _ in range(student_input):
+        await Estudiante(
+            name=fk.name(),
+            edad=fk.random_int(min=18, max=120),
+            ciudad=fk.city()
+        ).create()
+
+    modalidades_posibles = ["presencial", "híbrido", "aula virtual", "online"]
+    for _ in range(curso_input):
+        await Curso(
+            name=fk.sentence(3),
+            duration=fk.random_int(min=25, max=520),
+            modalidad=modalidades_posibles[fk.random_int(min=0, max=3)]
+        ).create()
+
+
+if __name__ == "__main__":
+    asyncio.run(seed_everything())    
+
+    
