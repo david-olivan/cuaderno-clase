b6a8c1313959b35780a110b19d4945ec4166ea13
David Oliván Malagón <david.olivan.93@gmail.com>
2025-07-30 13:53:44 +0200

    feat: hasheo de apy keys, validación y dependencias FastAPI

--- 4-schoolapi v1/src/main.py
+++ 4-schoolapi v1/src/main.py
@@ -1,5 +1,5 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Depends
 from .database import init_db, Estudiante, Curso
 from beanie import PydanticObjectId
 from pydantic import BaseModel
@@ -41,10 +41,13 @@ async def get_cursos():
     cursos = await Curso.find_all().to_list()
     return cursos
 
+endpoint_auth = {403: {}, 401: {}}
 
-@app.post("/cursos", tags=["Cursos"], status_code=201)
-async def crear_curso(curso: CursoRequest):    
-    nuevo_curso = await Curso(**curso.model_dump()).create()
-    return {"message": "Curso creado correctamente", "data": nuevo_curso}
+@app.post("/cursos", tags=["Cursos"], status_code=201, responses=endpoint_auth) # type: ignore
+async def crear_curso(curso: CursoRequest, role: str = Depends(auth.get_api_key_from_headers)):
+    if role == "admin":
+        nuevo_curso = await Curso(**curso.model_dump()).create()
+        return {"message": "Curso creado correctamente", "data": nuevo_curso}
+    raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Tu rol actual no permite la creación de cursos")
 
 app.include_router(estudiantes.router)
--- 4-schoolapi v1/src/routers/auth.py
+++ 4-schoolapi v1/src/routers/auth.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from pydantic import BaseModel
 import secrets
+import bcrypt
+from typing import Optional
 
 from ..database import API_Keys 
 
+
+security = HTTPBearer()
 
 class APIRequest(BaseModel):
     role: str
@@ -18,6 +23,40 @@ roles = ["admin", "user", "test"]
 
 router = APIRouter(prefix="/auth", tags=["API Keys"])
 
+
+
+
+async def generate_new_key(role: str) -> str:
+    raw_key = "S-API-" + secrets.token_urlsafe(32)
+
+    # Hashearé todo, y lo guardaré en la base de datos
+    salt = bcrypt.gensalt()
+    hashed_key = bcrypt.hashpw(raw_key.encode(), salt)
+    await API_Keys(api_key=hashed_key.decode(), role=role).create()
+
+    return raw_key
+
+
+async def validate_api_key(raw_key: str) -> Optional[str]:
+    keys = await API_Keys.find_all().to_list()
+
+    for item in keys:
+        # Comparar api keys
+        if bcrypt.checkpw(raw_key.encode(), item.api_key.encode()):
+            return item.role
+        
+    raise HTTPException(401, detail="Pasame una API Key válida")
+
+
+async def get_api_key_from_headers(credentials: HTTPAuthorizationCredentials = Depends(security)) -> Optional[str]:
+    if credentials is None:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Es necesaria una API Key para usar este endpoint")
+    
+    role = await validate_api_key(credentials.credentials)
+
+    return role
+
+
 @router.post("/new", status_code=status.HTTP_201_CREATED, response_model=APIResponse)
 async def create_new_key(request: APIRequest):
     if request.role not in roles:
@@ -31,23 +70,6 @@ async def create_new_key(request: APIRequest):
 
 
 @router.get("/me")
-async def read_role(api_key: str):
-    # Validar la api_key y devolver el role del usuario
-    role = validate_api_key()
-
+async def read_role(role: str = Depends(get_api_key_from_headers)):
     return { "message": f"Tu rol es {role}."}
 
-
-# TODO implementar
-async def generate_new_key(role: str) -> str:
-    raw_key = "S-API-" + secrets.token_urlsafe(32)
-
-    # Hashearé todo, y lo guardaré en la base de datos
-    # TODO hashear y almacenar el hash con salt
-    await API_Keys(api_key=raw_key, role=role).create()
-
-    return raw_key
-
-
-# TODO implementar
-async def validate_api_key(): ...
