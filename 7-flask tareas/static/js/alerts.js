36deebe90f913b15c65497462c2f32647ab78dd5
David Oliván Malagón <david.olivan.93@gmail.com>
2025-09-09 14:03:30 +0200

    feat: Miniapp de tareas con completar y eliminar

--- 7-flask tareas/static/js/alerts.js
+++ 7-flask tareas/static/js/alerts.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const alerts = document.querySelectorAll('.alert');
     
     alerts.forEach(alert => {
-        // Agregar la clase que iniciará la animación después de 3 segundos
+        // Agregar la clase que iniciará la animación después de 1,5 segundos
         setTimeout(() => {
             alert.classList.add('fade-out');
         }, 1500);
@@ -10,6 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
         // Remover el elemento después de que la animación termine
         setTimeout(() => {
             alert.remove();
-        }, 2000); // 3000ms + 500ms de la animación
+        }, 2000); // 1500ms + 500ms de la animación
     });
 });
